9b40892f4797eba830ef6f698ab59871a983159c
Chill Magic <hanlengmowang@hotmail.com>
2018-02-03 00:24:53 +0800

    Add 'Convert::to_integer(char, ...)' and 'Convert::to_hex(void*,size_t)'

--- source/convert.cpp
+++ source/convert.cpp
@@ -1,9 +1,37 @@
 #include "convert.h"
 #include "charptr.h"
 #include "record.h"
+#include "range.h"
 #include <algorithm>
 
 PRILIB_BEGIN
+namespace Convert
+{
+	std::string to_hex(void *src, size_t size)
+	{
+		charptr result(3 * size + 10);
+
+		byte *begin = reinterpret_cast<byte*>(src);
+		byte *end = begin + size;
+		char *rp = result;
+
+		for (auto ptr : range(begin, end)) {
+			int v;
+			v = *ptr / 0x10;
+			rp[0] = (v < 10) ? ('0' + v) : ('A' + v - 10);
+			v = *ptr % 0x10;
+			rp[1] = (v < 10) ? ('0' + v) : ('A' + v - 10);
+			rp[2] = ' ';
+			rp += 3;
+		}
+
+		if (size > 0)
+			rp[-1] = '\0';
+
+		return result.to_string();
+	}
+}
+
 namespace Convert
 {
 	template <typename T, T f(const char *str, char **str_end, int base)>
@@ -35,17 +63,48 @@ namespace Convert
 		return to_num_base<unsigned long long, std::strtoull>(str, result, base);
 	}
 
-	static bool to_base16(char c, uint8_t &result) {
-		if (isxdigit(c)) {
-			result = (isdigit(c)) ? (c - '0') : (c - 'a' + 0xa);
+	static int to_integer(char c);
+
+	bool is_integer(char c, int base)
+	{
+		int r = to_integer(c);
+		return r >= 0 && r < base;
+	}
+
+	bool is_integer(const std::string &str, int base)
+	{
+		for (auto &c : str) {
+			if (!is_integer(c, base))
+				return false;
+		}
+		return true;
+	}
+
+	static int to_integer(char c)
+	{
+		if (isdigit(c))
+			return c - '0';
+		else if (islower(c))
+			return c - 'a' + 0xa;
+		else if (isupper(c))
+			return c - 'A' + 0xa;
+		else
+			return -1;
+	}
+
+	bool to_integer(char c, int &result, int base)
+	{
+		if (is_integer(c, base)) {
+			result = to_integer(c);
 			return true;
 		}
+
 		return false;
 	}
 
 	static bool to_base16(char higher, char lower, uint8_t &result) {
 		uint8_t v1, v2;
-		if (to_base16(higher, v1) && to_base16(lower, v2)) {
+		if (to_integer(higher, v1, 16) && to_integer(lower, v2, 16)) {
 			result = v1 * 0x10 + v2;
 			return true;
 		}
@@ -77,7 +136,7 @@ namespace Convert
 		}
 
 		if ((isresize || (!isresize && size > strsize / 2)) && (strsize % 2 != 0 && (islsb || (!islsb && msize > i)))) {
-			if (!to_base16(str[0], reinterpret_cast<uint8_t*>(dst)[islsb ? i : (msize - i - 1)]))
+			if (!to_integer(str[0], reinterpret_cast<uint8_t*>(dst)[islsb ? i : (msize - i - 1)], 16))
 				return false;
 		}
 		return true;
