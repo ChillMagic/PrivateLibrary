1e7242f98f1f0998833958757f8cab9cfd95f025
Chill Magic <hanlengmowang@hotmail.com>
2018-02-02 20:52:02 +0800

    Add 'Convert::to_base16'

--- source/convert.cpp
+++ source/convert.cpp
@@ -1,6 +1,7 @@
 #include "convert.h"
 #include "charptr.h"
 #include "record.h"
+#include <algorithm>
 
 PRILIB_BEGIN
 namespace Convert
@@ -32,6 +33,49 @@ namespace Convert
 	bool to_ull(const std::string &str, unsigned long long &result, int base)
 	{
 		return to_num_base<unsigned long long, std::strtoull>(str, result, base);
+	}
+
+	static bool to_base16(char c, uint8_t &result) {
+		if (isxdigit(c)) {
+			result = (isdigit(c)) ? (c - '0') : (c - 'a' + 0xa);
+			return true;
+		}
+		return false;
+	}
+
+	static bool to_base16(char higher, char lower, uint8_t &result) {
+		uint8_t v1, v2;
+		if (to_base16(higher, v1) && to_base16(lower, v2)) {
+			result = v1 * 0x10 + v2;
+			return true;
+		}
+		return false;
+	}
+
+	bool to_base16(const std::string &str, void *dst, size_t size) {
+		if (str.empty())
+			return false;
+
+		bool isresize = size == 0;
+
+		const size_t strsize = str.size();
+		if (isresize)
+			size = strsize / 2 + strsize % 2;
+
+		for (size_t i = 0; i < std::min(strsize / 2, size); ++i) {
+			auto &d = reinterpret_cast<uint8_t*>(dst)[i];
+			size_t si = strsize - i * 2 - 2;
+			char higher = str[si];
+			char lower = str[si + 1];
+			if (!to_base16(higher, lower, d))
+				return false;
+		}
+
+		if ((isresize || (!isresize && size > strsize / 2)) && (strsize % 2 != 0)) {
+			if (!to_base16(str[0], reinterpret_cast<uint8_t*>(dst)[strsize / 2]))
+				return false;
+		}
+		return true;
 	}
 }
 
