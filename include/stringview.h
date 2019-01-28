eca61d594f40ffd7b77997bc02b708f656e2ac66
Chill Magic <hanlengmowang@hotmail.com>
2019-01-28 12:27:42 +0800

    Remove length for StringView

--- include/stringview.h
+++ include/stringview.h
@@ -11,29 +11,25 @@ PRILIB_BEGIN
 class StringView
 {
 public:
-	StringView(const char *str, size_t length = 0) : _pointer(str), _length(length) {}
-	StringView(const std::string &str) : _pointer(str.data()), _length(str.length()) {}
-	StringView(const PriLib::charptr &cp) : _pointer(cp.get()), _length(cp.length()) {}
+	StringView(const char *str) : _pointer(str) {}
+	StringView(const std::string &str) : _pointer(str.data()) {}
+	StringView(const PriLib::charptr &cp) : _pointer(cp.get()) {}
 
-	size_t length() const {
-		if (_length == 0 && _pointer) {
-			return std::strlen(_pointer);
-		}
-		return _length;
-	}
-	size_t size() const {
-		return length();
-	}
 	const char* get(size_t offset = 0) const {
 	    return _pointer + offset;
 	}
 	char operator[](size_t offset) {
 	    return _pointer[offset];
 	}
+	StringView offset(size_t offset) {
+		return StringView(_pointer + offset);
+	}
+	StringView offset(ptrdiff_t offset) {
+		return StringView(_pointer + offset);
+	}
 
 private:
 	const char *_pointer = nullptr;
-	mutable size_t _length = 0;
 };
 PRILIB_END
 
