13ece87b93ca3ff8ae709053466ac99c3b932d7c
Chill Magic <hanlengmowang@hotmail.com>
2019-01-29 14:37:56 +0800

    Add some methods for StringView

--- include/stringview.h
+++ include/stringview.h
@@ -6,31 +6,46 @@
 #define _PRILIB_STRINGVIEW_H_
 #include "macro.h"
 #include "charptr.h"
+#include <cassert>
 
 PRILIB_BEGIN
 class StringView
 {
 public:
-	StringView(const char *str) : _pointer(str) {}
-	StringView(const std::string &str) : _pointer(str.data()) {}
-	StringView(const PriLib::charptr &cp) : _pointer(cp.get()) {}
+	using OffsetType = size_t;
+public:
+	StringView(const char *str, OffsetType offset = 0) : _pointer(str + offset) {}
+	StringView(const std::string &str, OffsetType offset = 0) : _pointer(str.data() + offset) {}
+	StringView(const PriLib::charptr &cp, OffsetType offset = 0) : _pointer(cp.get() + offset) {}
 
-	const char* get(size_t offset = 0) const {
+	const char* get(OffsetType offset = 0) const {
+		assert(_pointer);
 	    return _pointer + offset;
 	}
-	char operator[](size_t offset) {
+	char operator[](OffsetType offset) const {
+		assert(_pointer);
 	    return _pointer[offset];
 	}
-	StringView offset(size_t offset) {
-		return StringView(_pointer + offset);
+	StringView& operator+=(OffsetType offset) {
+		assert(_pointer);
+		this->_pointer += offset;
+		return *this;
 	}
-	StringView offset(ptrdiff_t offset) {
-		return StringView(_pointer + offset);
+	std::string toString() const {
+		return std::string(_pointer);
+	}
+	std::string toSubString(OffsetType offset = 0) const {
+		return std::string(_pointer, _pointer + offset);
 	}
 
 private:
 	const char *_pointer = nullptr;
 };
+
+inline std::string toSubString(const StringView &lhs, const StringView &rhs) {
+	assert(lhs.get() < rhs.get());
+	return lhs.toSubString(rhs.get() - lhs.get());
+}
 PRILIB_END
 
 #endif
